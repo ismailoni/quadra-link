fafe13615a3df49ec353ce1eb146217ab1931992
Oni Ismail Modupe <ismailoni80@gmail.com>
2025-09-18 15:31:25 +0100

    feat: implement user roles and guards, enhance app module configuration

--- quadra-link-be/src/app.module.ts
+++ quadra-link-be/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // ⚠️ true for dev only, disable in production!
+      }),
+    }),
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
---
+++ quadra-link-be/src/common/enums/user-role.enum.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+
+export enum UserRole {
+  USER = 'user',
+  MODERATOR = 'moderator',
+  COUNSELLOR = 'counsellor',
+  ADMIN = 'admin',
+}
